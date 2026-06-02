d86913e3c541f4df46dd051ccb2404c4da4ae21e
Genesis Ulin <genesis89ulin@gmail.com>
2026-06-01 22:46:52 -0500

    Clean up comments in working_gauges.py
    Removed commented-out code regarding site name retrieval.

--- working_gauges.py
+++ working_gauges.py
@@ -1,6 +1,5 @@
 import requests
 
-# This URL works - we already tested it
 url = "https://www.harriscountyfws.org/Home/GetSiteRecentData?regionId=3&regionId=24&regionId=25&regionId=26&regionId=21&timeSpan=7&dt=1778349600000"
 
 headers = {"Referer": "https://www.harriscountyfws.org/"}
@@ -14,8 +13,6 @@ for site in sites[:10]:
     props = site["properties"]
     site_id = props.get("SiteId", "Unknown")
     
-    # The site name isn't in this endpoint, but we have the ID
-    # Let's see what other info we can use
     stream = props["StreamData"][0]
     current = stream["CurrentLevel"]
     flood = stream["ChannelInfo"]["FloodLevelIndicator"]
